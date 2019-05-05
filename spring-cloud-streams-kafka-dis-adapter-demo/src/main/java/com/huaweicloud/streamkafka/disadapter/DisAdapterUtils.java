3bc11190ecdfd4bf64e66f9ad969f3b1e7623b2a
jzc928 <jzc928@163.com>
2019-05-05 17:54:22 +0800

    Update DisAdapterUtils.java

--- spring-cloud-streams-kafka-dis-adapter-demo/src/main/java/com/huaweicloud/streamkafka/disadapter/DisAdapterUtils.java
+++ spring-cloud-streams-kafka-dis-adapter-demo/src/main/java/com/huaweicloud/streamkafka/disadapter/DisAdapterUtils.java
@@ -7,11 +7,10 @@ public class DisAdapterUtils
     
     public static DISConfig buildDisConfig(){
         return new DISConfig()
-        .setEndpoint("https://128.10.96.207:21241")
-        .set("manager.endpoint", "https://128.10.96.207:21102")
-        .setAK("CWQCEIGWRDFYNYTHXVBD")
-        .setSK("hWGwgAsWdqinap7TlgYiqxsGGtP2rADP9UXWPNlV")
-        .setProjectId("2fdbbc096fb0420489021acc8b802105")
+        .setEndpoint("https://x.x.x.x:xx")
+        .setAK("xxx")
+        .setSK("xxxx")
+        .setProjectId("xxxxx")
         .setRegion("southchina")
         .set("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
         .set("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
